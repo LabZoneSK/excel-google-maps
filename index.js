843241b3c01ca8d0b46e96241a2862bd274cfe37
Martin Starosta <martin.starosta83@gmail.com>
2018-03-13 00:10:42 +0100

    Create simple snippet

---
+++ index.js
@@ -0,0 +1,57 @@
+const compression = require('compression');
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const API_VERSION = process.env.API_VERSION || 'v1';
+
+const app = express();
+
+const Client = require('node-rest-client').Client;
+const client = new Client();
+
+const getLocation = (address) => {
+  return new Promise((resolve, reject) => {
+    client.get('https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDZect4UgqzVPxbSYQJDb8x4QVx9DFyCPE&address=Košice', (data, response) => {
+      const result = data.results.shift();
+      if(result.geometry) {
+        resolve(result.geometry.location);
+      } else {
+        reject('Wrong address: Address not found by Google Maps API.');
+      }
+    });
+  });
+}
+
+const getDistance = (origins, destinations) => {
+  return new Promise((resolve, reject) => {
+    const encodedURL = encodeURI(`https://maps.googleapis.com/maps/api/distancematrix/json?key=AIzaSyDZect4UgqzVPxbSYQJDb8x4QVx9DFyCPE&origins=${origins}&destinations=${destinations}`);
+    client.get(encodedURL, (data, response) => {
+      const element = data.rows.shift().elements;
+      if(element) {
+        const distance = element.shift().distance.value / 1000;
+        resolve(distance);
+      } else {
+        reject('Wrong address: Address not found by Google Maps API.');
+      }
+    });
+  });
+}
+
+app.get(`/api/${API_VERSION}/distance/:source/:destination`, (req, res) => {
+
+  const { source, destination } = req.params;
+  getDistance(source, destination).then((distance) => {
+    res.send(`${distance}`);
+  })
+  /*
+  const sourceLocation = getLocation(source);
+  const destinationLocation = getLocation(destination);
+  
+  Promise.all([ sourceLocation, destinationLocation]).then((locations) => {
+    console.log(locations[0].lat);
+    res.send(locations);
+  });
+  */
+});
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
